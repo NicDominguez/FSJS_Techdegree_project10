1b77e29e2bba25a5a21b26964e56ea9c4843c78f
NicDominguez <NicDominguez@users.noreply.github.com>
2020-04-13 15:48:12 -0700

    removed boiler plate logo and componenet

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+
+//Test Case
+const courseData = fetch(`http://localhost:5000/courses`).then(res => res.json()).catch(console.log)
+
+console.log(courseData)
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <ul>check console</ul>
   );
 }
 
